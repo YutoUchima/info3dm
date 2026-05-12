04b5c08ba44f590a1a12687c55d6904ea567c14b
Yuto Uchima <e245756@ie.u-ryukyu.ac.jp>
2026-05-12 11:04:05 +0900

    Week4 ex4 ex1.7

---
+++ ex4/learn1.py
@@ -0,0 +1,18 @@
+import dataset1
+
+# ex1.1
+dataset1.ex1_1()
+
+# ex1.2
+df = dataset1.ex1_2()
+
+# ex1.3
+df = dataset1.ex1_3(df)
+
+# ex1.4
+dataset1.ex1_4(df)
+
+# ex1.5
+loaded_df = dataset1.ex1_5()
+
+print(loaded_df)
